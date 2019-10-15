b410482cf7361bd6175eb19067b0caef63b5dc62
Bvlion <bvltiggeari@ambitious-i.net>
2019-10-15 12:08:39 +0900

    developへのコミットでテストと結果アップロードをCircleCIで実施

---
+++ report/functions/index.js
@@ -0,0 +1,13 @@
+const functions = require('firebase-functions')
+const express = require('express')
+const basicAuth = require('basic-auth-connect')
+
+const app = express()
+
+app.all('/*', basicAuth(function(user, password) {
+  return user === "user" && password === "pass";
+}));
+
+app.use(express.static(__dirname + '/static/'))
+
+exports.app = functions.https.onRequest(app)
